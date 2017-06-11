d1fa21f4b94b46df4b16c94af05a30064ee485e2
Ludis <pjgvanschaik@gmail.com>
2017-06-11 15:44:58 +0200

    Create upgrade.py

--- upgrade.py
+++ upgrade.py
@@ -1,5 +1,5 @@
 import sys, os
 os.system("apt-get update && apt-get upgrade && apt-get dist-upgrade")
-if(str(input('Reboot [Y/N]:')) == 'Y'): os.system("reboot")
+if(str(input('Reboot [Y/N]:')) == in ['Y', 'y']): os.system("reboot")
 
 
