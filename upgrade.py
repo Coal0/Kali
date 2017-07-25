015ac36fd6ec8eed6bd05fb6d9735699b0665a02
Coal0 <Coal0@users.noreply.github.com>
2017-07-25 15:47:18 +0200

    Update upgrade.py
    * Better error reporting;
    * Python 2/3 compatibility;
    * Cleaner system calls

--- upgrade.py
+++ upgrade.py
@@ -1,3 +1,19 @@
-import sys, os
-os.system("apt-get update && apt-get upgrade && apt-get dist-upgrade")
-if(str(input('Reboot [Y/N]:')) == in ['Y', 'y']): os.system("reboot")
+import sys
+import subprocess
+
+if __name__ == "__main__":
+    print("Upgrading...")
+    try:
+        subprocess.check_call(
+          "apt-get update && apt-get upgrade && apt-get dist-upgrade",
+          shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    except subprocess.CalledProcessError:
+        print("Upgrade failed, try again.")
+    else:
+        print("Upgrade successful.")
+        if sys.version_info.major < 3:
+            if raw_input("Reboot [Y/N]: ") in ("Y", "y"):
+                subprocess.call("reboot", shell=True)
+        else:
+            if input("Reboot [Y/N]: ") in ("Y", "y"):
+                subprocess.call("reboot", shell=True)
